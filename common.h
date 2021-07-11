278b0f2f7ba72114685c3e2643dc795e334e65a1
Oliver Calder <olivercalder@protonmail.ch>
2021-07-10 22:36:47 -0500

    Added uintN_t to common.h, since <stdint.h> is in C99, not C89.

--- common.h
+++ common.h
@@ -6,8 +6,16 @@
 #define FONT_WIDTH      4
 #define BUFSIZE         (TERM_WIDTH / FONT_WIDTH)
 
-#define FMT_TXT         0
-#define FMT_RAW         1
+#define FMT_RAW         0
+#define FMT_UINT        1
+#define FMT_TXT_HEX     2
+#define FMT_TXT_LOWER   3
+#define FMT_TXT_UPPER   4
+
+typedef unsigned char   uint8_t;
+typedef unsigned short  uint16_t;
+typedef unsigned long   uint32_t;
+typedef unsigned long   long uint64_t;
 
 int min_int(int a, int b);
 
